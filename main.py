b8f076fdea8e673826e97926a148e8353c2b9659
DEVIPRASAD SINGH SAMANT <deviprasadsinghsamant@gmail.com>
2025-09-03 17:57:26 +0530

    Refactor import statements in main.py for clarity

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
-from . import models, schemas, database
+import models
+import schemas
+import database
 
 app = FastAPI()
 
