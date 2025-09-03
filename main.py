a62d2be2b5db6aa8d478d3916d50d8cb0ac9ee2d
DEVIPRASAD SINGH SAMANT <deviprasadsinghsamant@gmail.com>
2025-09-03 17:51:52 +0530

    Second commit

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from . import models, schemas, database
 
 app = FastAPI()
 
 @app.get("/")
 def say_hello():
     return {"message": "NextCare Bolte!"}
+
+@app.post("/register")
+def register_user(user: schemas.UserCreate, db: Session = Depends(database.get_db)):
+
+    existing_user = db.query(models.User).filter(models.User.username == user.username).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Username already exists")
+
+    new_user = models.User(username=user.username, password=user.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {"message": "User registered successfully", "user_id": new_user.id}
